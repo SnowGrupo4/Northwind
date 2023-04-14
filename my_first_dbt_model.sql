5580bd9230ffdb247dae2e9a83f187c3ffaa07a3
SnowGrupo4 <130581715+SnowGrupo4@users.noreply.github.com>
2023-04-13 23:06:57 -0600

    Secondary Branch

--- my_first_dbt_model.sql
+++ my_first_dbt_model.sql
@@ -1,9 +1,5 @@
 
-  
-    
-
-        create or replace transient table STAGE.ELT.my_first_dbt_model  as
-        (/*
+/*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
@@ -11,7 +7,7 @@
     Try changing "table" to "view" below
 */
 
-
+{{ config(materialized='table') }}
 
 with source_data as (
 
@@ -29,6 +25,3 @@ from source_data
 */
 
 -- where id is not null
-        );
-      
-  
